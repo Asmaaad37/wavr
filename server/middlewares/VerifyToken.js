56a5fb7aa15cb7222980bceac26d23b6503e8a01
Asmaad <asmaadsaeed03@gmail.com>
2026-05-30 16:45:20 +0500

    fix: guard against missing Authorization header in VerifyToken

--- server/middlewares/VerifyToken.js
+++ server/middlewares/VerifyToken.js
@@ -1,31 +1,35 @@
 import auth from "../config/firebase-config.js";
 
 export const VerifyToken = async (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized: missing token" });
+  }
+
+  const token = authHeader.split(" ")[1];
 
   try {
     const decodeValue = await auth.verifyIdToken(token);
-    if (decodeValue) {
-      req.user = decodeValue;
-      return next();
-    }
+    req.user = decodeValue;
+    return next();
   } catch (e) {
-    return res.json({ message: "Internal Error" });
+    return res.status(401).json({ message: "Unauthorized: invalid token" });
   }
 };
 
 export const VerifySocketToken = async (socket, next) => {
   const token = socket.handshake.auth.token;
 
+  if (!token) {
+    return next(new Error("Unauthorized: missing token"));
+  }
+
   try {
     const decodeValue = await auth.verifyIdToken(token);
-
-    if (decodeValue) {
-      socket.user = decodeValue;
-
-      return next();
-    }
+    socket.user = decodeValue;
+    return next();
   } catch (e) {
-    return next(new Error("Internal Error"));
+    return next(new Error("Unauthorized: invalid token"));
   }
 };
